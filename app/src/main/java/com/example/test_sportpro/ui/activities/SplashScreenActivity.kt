cbc7f34f47d9e94e2f95163007ec88eed45b1d3a
Elnura Rashidova <elnura766@gmail.com>
2021-06-04 15:30:08 +0600

    changed splash screen

--- app/src/main/java/com/example/test_sportpro/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/test_sportpro/ui/activities/SplashScreenActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.test_sportpro.R
 
 class SplashScreenActivity : AppCompatActivity() {
-    private var TIME_OUT:Long = 3000
+    private var TIME_OUT:Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +20,18 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun loadSplashScreen(){
+        val image = findViewById<ImageView>(R.id.logo_image)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            // You can declare your desire activity here to open after finishing splash screen. Like MainActivity
+            image.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+            
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         },TIME_OUT)
     }
+
+
 }
