69d0171439a62b895a5d3623c9e9d2c33536319c
azamat229 <azamatmambetaliev229@gmail.com>
2021-06-12 16:31:32 +0600

    feat: Correction of the authorization and registration section

---
+++ app/src/main/java/com/example/test_sportpro/ConfirmationFragment.kt
@@ -0,0 +1,25 @@
+package com.example.test_sportpro
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ConfirmationFragment : Fragment(),View.OnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
--- app/src/main/java/com/example/test_sportpro/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/test_sportpro/ui/fragments/ProfileFragment.kt
@@ -1,111 +1,64 @@
 package com.example.test_sportpro.ui.fragments
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.test_sportpro.R
 import com.example.test_sportpro.databinding.FragmentProfileBinding
 import com.example.test_sportpro.ui.NewsViewModel
 import com.example.test_sportpro.ui.activities.MainActivity
-import com.jgabrielfreitas.core.BlurImageView;
 
 
-class ProfileFragment : Fragment(R.layout.fragment_profile){
+class ProfileFragment : Fragment(R.layout.fragment_profile),View.OnClickListener {
 
+    lateinit var navController: NavController
     lateinit var viewModel: NewsViewModel
     var num = 19
     private var fragmentProfileBinding: FragmentProfileBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<ImageButton>(R.id.imageButtonJudge).setOnClickListener(this)
+
         fragmentProfileBinding = FragmentProfileBinding.bind(view)
         viewModel = (activity as MainActivity).viewModel
 
-//        fragmentProfileBinding?.imageView?.setBlur(num)
-
         var navc: NavController? = null
         navc = Navigation.findNavController(view)
-        fragmentProfileBinding!!.imageButtonJudge.setOnClickListener(){
-//            navc?.navigate.(R.id.action_ProfileFragment)
-        }
-
-//        fragmentProfileBinding!!.dogBlurImageView.setBlur(20)
-
-        fragmentProfileBinding!!.imageButtonJudge.setOnClickListener() {
-            hideFirst()
-            showNumber()
 
 
-        }
-
-        fragmentProfileBinding!!.imageButtonTrainer.setOnClickListener() {
-            hideFirst()
-            showNumber()
-        }
-
-        fragmentProfileBinding!!.buttonNum.setOnClickListener() {
-//            activity?.let {
-//                val intent = Intent(it, Home::class.java)
-//                it.startActivity(intent)
-//            }
-            hideFirst()
-            hideNumber()
-            showCode()
-
-        }
-
-
-        fragmentProfileBinding!!.buttonCode.setOnClickListener() {
-            hideFirst()
-            hideNumber()
-            hideCode()
-            showLast()
-        }
-    }
-
-    private fun hideFirst() {
-
-
-        fragmentProfileBinding?.linearLayoutFirst?.visibility = View.INVISIBLE
-    }
-
-    private fun hideNumber() {
-        fragmentProfileBinding?.linearLayoutNumber?.visibility = View.INVISIBLE
-    }
-
-    private fun hideCode() {
-        fragmentProfileBinding?.linearLayoutCode?.visibility = View.INVISIBLE
     }
 
 
-    private fun showFirst() {
-        fragmentProfileBinding?.linearLayoutFirst?.visibility = View.VISIBLE
-    }
-
-    private fun showNumber() {
-        fragmentProfileBinding?.linearLayoutNumber?.visibility = View.VISIBLE
-    }
-
-    private fun showCode() {
-        fragmentProfileBinding?.linearLayoutCode?.visibility = View.VISIBLE
-    }
-
-    private fun showLast() {
-        fragmentProfileBinding?.linearLayoutLast?.visibility = View.VISIBLE
-    }
 
     override fun onResume() {
         super.onResume()
         (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
     }
+
     override fun onStop() {
         super.onStop()
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
     }
 
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.imageButtonJudge -> navController!!.navigate(R.id.action_profileFragment_to_numberFragment3)
+
+        }
+    }
 }
